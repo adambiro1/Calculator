f1877f0cf7fa180811e7ffcd6c6ea5e0c879f793
adambiro1 <adambiro@tutanota.com>
2022-04-29 19:33:24 +0200

    pridal tukanie cisel, znaciek musim

--- app.js
+++ app.js
@@ -1,4 +1,108 @@
-let result;
-let number1;
-let operator;
-let number2;
+//let result;
+//let number1;
+//let operator;
+//let number2;
+
+let number1 = document.getElementById("1").onclick = get1;
+function get1(){
+    document.getElementById("number1").innerHTML = 1;
+}
+document.getElementById("2").onclick = get2;
+function get2(){
+    document.getElementById("number1").innerHTML = 2;
+}    
+document.getElementById("3").onclick = get3;
+function get3(){
+    document.getElementById("number1").innerHTML = 3;
+}
+document.getElementById("4").onclick = get4;
+function get4(){
+    document.getElementById("number1").innerHTML = 4;
+}
+document.getElementById("5").onclick = get5;
+function get5(){
+    document.getElementById("number1").innerHTML = 5;
+}
+document.getElementById("6").onclick = get6;
+function get6(){
+    document.getElementById("number1").innerHTML = 6;
+}
+document.getElementById("7").onclick = get7;
+function get7(){
+    document.getElementById("number1").innerHTML = 7;
+}
+document.getElementById("8").onclick = get8;
+function get8(){
+    document.getElementById("number1").innerHTML = 8;
+}
+document.getElementById("9").onclick = get9;
+function get9(){
+    document.getElementById("number1").innerHTML = 9;
+}
+document.getElementById("0").onclick = get0;
+function get0(){
+    document.getElementById("number1").innerHTML = 0;
+}
+
+
+let number2 = document.getElementById("1").onclick = get1;
+function get1(){
+    document.getElementById("number2").innerHTML = 1;
+}
+document.getElementById("2").onclick = get2;
+function get2(){
+    document.getElementById("number2").innerHTML = 2;
+}    
+document.getElementById("3").onclick = get3;
+function get3(){
+    document.getElementById("number2").innerHTML = 3;
+}
+document.getElementById("4").onclick = get4;
+function get4(){
+    document.getElementById("number2").innerHTML = 4;
+}
+document.getElementById("5").onclick = get5;
+function get5(){
+    document.getElementById("number2").innerHTML = 5;
+}
+document.getElementById("6").onclick = get6;
+function get6(){
+    document.getElementById("number2").innerHTML = 6;
+}
+document.getElementById("7").onclick = get7;
+function get7(){
+    document.getElementById("number2").innerHTML = 7;
+}
+document.getElementById("8").onclick = get8;
+function get8(){
+    document.getElementById("number2").innerHTML = 8;
+}
+document.getElementById("9").onclick = get9;
+function get9(){
+    document.getElementById("number2").innerHTML = 9;
+}
+document.getElementById("0").onclick = get0;
+function get0(){
+    document.getElementById("number2").innerHTML = 0;
+}
+
+let operator = document.getElementById("+").onclick = getOperatorPlus;
+function getOperatorPlus(){
+    document.getElementById("operator").innerHTML = "+";
+}
+document.getElementById("-").onclick = getOperatorMinus;
+function getOperatorMinus(){
+    document.getElementById("operator").innerHTML = "-";
+}
+document.getElementById("*").onclick = getOperatorMultiple;
+function getOperatorMultiple(){
+    document.getElementById("operator").innerHTML = "*";
+}
+document.getElementById("/").onclick = getOperatorDivide;
+function getOperatorDivide(){
+    document.getElementById("operator").innerHTML = "/";
+}
+
+// sprav aby sa ti nemenilo iba jedno číslo a vyrieš matiku.
+
+
