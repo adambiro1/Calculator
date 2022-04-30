95ba0ae492adda9beafeacc513a6cd04efbac275
adambiro1 <adambiro@tutanota.com>
2022-04-30 21:02:08 +0200

    Upravil Variabels

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.form1.textview.value = document.form1.textview.value + num;
 
 function equal()
 {
-var exp = document.form1.textview.value;
+let exp = document.form1.textview.value;
 if(exp)
 {
 document.form1.textview.value = eval(exp)
@@ -15,7 +15,7 @@ document.form1.textview.value = eval(exp)
 function backspace()
 {
 //clear function
-var exp = document.form1.textview.value;
+let exp = document.form1.textview.value;
 document.form1.textview.value = exp.substring(0, exp.length - 1); 
 }
 
